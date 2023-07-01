5faf4328576b980746744556e1b518a415a7d826
Adamosssssss <137040636+Adamosssssss@users.noreply.github.com>
2023-07-01 23:02:35 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,8 +2,8 @@ import tkinter as tk
 import socket
 import threading
 from time import sleep
-from RSA import RSA
-
+import RSA
+import json
 
 class TicTacToeServer:
     def __init__(self):
@@ -16,6 +16,12 @@ class TicTacToeServer:
 
         self.window = tk.Tk()
         self.window.title("Tic-Tac-Toe Server")
+        
+        #RSA
+        primes = RSA.get_primes(15, 300)
+        self.PUBLIC_KEY, self.PRIVATE_KEY = RSA.keys(primes[0], primes[1])
+        self.public_keys = {}
+        print(self.PUBLIC_KEY)
 
         # Top frame consisting of two buttons widgets (i.e. btnStart, btnStop)
         self.topFrame = tk.Frame(self.window)
@@ -51,7 +57,7 @@ class TicTacToeServer:
 
         self.server.bind((self.host_IP, self.port))
         self.server.listen(2)  # server is listening for client connection
-
+        
         threading.Thread(target=self.accept_clients, args=(self.server, " ")).start()
 
         self.lblHost["text"] = "Address: " + self.host_IP
@@ -65,34 +71,47 @@ class TicTacToeServer:
 
                 # use a thread so as not to clog the gui thread
                 threading.Thread(target=self.send_receive_client_message, args=(client, addr)).start()
-
+            
     def send_receive_client_message(self, client_connection, client_ip_addr):
         client_msg = " "
-
+        client_connection.send(str(self.PUBLIC_KEY).encode())
         # send welcome message to client
-        self.client_name = client_connection.recv(4096).decode()
-        # print(self.client_name)
+        #self.client_name = RSA.decrypt(self.PRIVATE_KEY, json.loads(client_connection.recv(1024).decode()))
+        self.client_name = client_connection.recv(1024).decode()
+        print(self.client_name)
 
         if len(self.clients) < 2:
             client_connection.send("welcome1".encode())
+
+            # Receive the first client's public key
+            public_key_str = client_connection.recv(1024).decode()
+            self.public_keys[0] = eval(public_key_str)
+            print("Received public key from Client 1")
+            print(self.public_keys[0])
         else:
             client_connection.send("welcome2".encode())
 
+            # Receive the second client's public key
+            public_key_str = client_connection.recv(1024).decode()
+            self.public_keys[1] = eval(public_key_str)
+            print("Received public key from Client 2")
+            print(self.public_keys[1])
+                    
         self.clients_names.append(self.client_name)
         self.update_client_names_display(self.clients_names)  # update client names display
 
         if len(self.clients) > 1:
             sleep(1)
             symbols = ["O", "X"]
-
             # send opponent name and symbol
             self.clients[0].send(("opponent_name$" + self.clients_names[1] + "symbol" + symbols[0]).encode())
             self.clients[1].send(("opponent_name$" + self.clients_names[0] + "symbol" + symbols[1]).encode())
 
         while True:
             # get the player choice from received data
-            data = client_connection.recv(4096).decode()
-            print(data)
+            data = client_connection.recv(1024).decode()
+            #data = RSA.decrypt(self.PRIVATE_KEY, json.loads(client_connection.recv(1024).decode()))
+            #print(data)
             if not data:
                 break
 
@@ -101,10 +120,17 @@ class TicTacToeServer:
                 # is the message from client1 or client2?
                 if client_connection == self.clients[0]:
                     # send the data from this player (client) to the other player (client)
+                    #enc_data = RSA.encrypt(self.public_keys[1], data)
+                    #self.clients[1].send(json.dumps(enc_data).encode('utf-8'))
+
                     self.clients[1].send(data.encode())
                 else:
                     # send the data from this player (client) to the other player (client)
+                    #enc_data = RSA.encrypt(self.public_keys[0], data)
+                    #self.clients[0].send(json.dumps(enc_data).encode('utf-8'))
+
                     self.clients[0].send(data.encode())
+
 
         # find the client index then remove from both lists(client name list and connection list)
         idx = self.get_client_index(self.clients, client_connection)
